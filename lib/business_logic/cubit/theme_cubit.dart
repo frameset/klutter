711797b17758b3ad9cfde21809571d84a52c4c40
Mark Winckle <mark@winckle.co.uk>
2021-07-16 14:16:44 +0100

    Persist theme choice between restarts

--- lib/business_logic/cubit/theme_cubit.dart
+++ lib/business_logic/cubit/theme_cubit.dart
@@ -1,8 +1,8 @@
-import 'package:bloc/bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 
 part 'theme_state.dart';
 
-class ThemeCubit extends Cubit<ThemeState> {
+class ThemeCubit extends HydratedCubit<ThemeState> {
   ThemeCubit() : super(ThemeState.system);
 
   setTheme(ThemeState newstate) {
@@ -10,9 +10,14 @@ class ThemeCubit extends Cubit<ThemeState> {
   }
 
   @override
-  void onChange(Change<ThemeState> change) {
-    // TODO: implement onChange
-    super.onChange(change);
-    print(change);
+  ThemeState? fromJson(Map<String, dynamic> json) {
+    return ThemeState.values.firstWhere((e) =>
+        e.toString().toLowerCase().split(".").last ==
+        json["value"].toLowerCase().split(".").last);
+  }
+
+  @override
+  Map<String, dynamic>? toJson(ThemeState state) {
+    return <String, String>{'value': state.toString()};
   }
 }
